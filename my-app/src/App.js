5e1fcb242dc4ae105010f8a40f48fa8da604b626
Kbceira <211043727@aluno.unb.br>
2022-06-17 16:41:35 -0300

    projeto

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TodoList from './componentes/TodoList';
+import Teste from './componentes/Teste';
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList  />
+    </div>
+  );
+}
+
+export default App;
